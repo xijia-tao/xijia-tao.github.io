9fbffbd42159276c6131ea4f036cbf7709c6ec2c
Ciel Tao <u3576776@connect.hku.hk>
2023-03-01 13:23:12 +0800

    use academic pages

--- from_drive.bat
+++
@@ -1,3 +0,0 @@
-ECHO This copies OneDrive content to local.
-PAUSE
-robocopy "D:\OneDrive - connect.hku.hk\Blog" content /E
--- from_local.bat
+++
@@ -1,3 +0,0 @@
-ECHO This copies local content to OneDrive
-PAUSE
-robocopy content "D:\OneDrive - connect.hku.hk\Blog" /E
